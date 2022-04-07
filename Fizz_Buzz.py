3e3cc3fd1b2f0f41c95d6369a02e434ce8ce522a
predybalov <79003995+predybalov@users.noreply.github.com>
2022-04-07 08:29:01 +0300

    Update task conditions

--- Fizz_Buzz.py
+++ Fizz_Buzz.py
@@ -1,31 +1,33 @@
-# Write a program that prints the numbers from 1 to 100.
-# But for multiples of three print "Fizz" instead of the number
-# and for the multiples of five print "Buzz". For numbers which
-# are multiples of both three and five print "FizzBuzz".
-#
-# Sample output:
-#
-# 1
-# 2
-# Fizz
-# 4
-# Buzz
-# Fizz
-# 7
-# 8
-# Fizz
-# Buzz
-# 11
-# Fizz
-# 13
-# 14
-# FizzBuzz
-# 16
-# 17
-# Fizz
-# 19
-# Buzz
-# ... etc up to 100
+"""
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number
+and for the multiples of five print "Buzz". For numbers which
+are multiples of both three and five print "FizzBuzz".
+
+Sample output:
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+... etc up to 100
+"""
 
 for num in range(1, 100+1):
     if (num % 5 == 0) and (num % 3 == 0):
